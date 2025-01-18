d74f554acdb83690a81661b4a13032110877c858
lpuygrenier <lpuygrenier@sqli.com>
2025-01-18 08:35:02 +0100

    FIX: Tests intégration

--- back/src/test/java/fr/el/g2l/G2lApplicationTests.java
+++ back/src/test/java/fr/el/g2l/G2lApplicationTests.java
@@ -3,8 +3,10 @@ package fr.el.g2l;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class G2lApplicationTests {
 
 	@Test
