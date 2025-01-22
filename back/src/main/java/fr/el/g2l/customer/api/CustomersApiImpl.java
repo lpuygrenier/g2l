16a3efead8eda55c5bb268e367336c0c1344362b
lpuygrenier <lpuygrenier@sqli.com>
2025-01-22 10:07:23 +0100

    feat: Ajout du /api en base du controller

--- back/src/main/java/fr/el/g2l/customer/api/CustomersApiImpl.java
+++ back/src/main/java/fr/el/g2l/customer/api/CustomersApiImpl.java
@@ -3,14 +3,16 @@ package fr.el.g2l.customer.api;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.el.g2l.customer.generated.api.CustomersApi;
 import fr.el.g2l.customer.generated.api.model.Customer;
 import fr.el.g2l.customer.internal.service.CustomerService;
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 @AllArgsConstructor
 public class CustomersApiImpl implements CustomersApi {
 
