075cc2975fc007070ae36e2e6c385e25d165eae2
lpuygrenier <56501291+lpuygrenier@users.noreply.github.com>
2025-02-22 07:27:06 +0000

    wip: schema

--- back/src/main/resources/db/changelog/1.0.0/01-init.sql
+++ back/src/main/resources/db/changelog/1.0.0/01-init.sql
@@ -1,69 +1,110 @@
--- --liquibase formatted sql
+-- liquibase formatted sql
 
--- --changeset lpuygrenier:01
--- CREATE SCHEMA customer;
--- CREATE SCHEMA referentiel;
--- CREATE SCHEMA rental;
+--changeset lpuygrenier:001
+CREATE TABLE conducteur (
+    id uuid PRIMARY KEY,
+    reference varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    first_name varchar(255) NOT NULL,
+    address varchar(255) NOT NULL,
+    city varchar(255) NOT NULL,
+    postal_code varchar(20) NOT NULL,
+    email varchar(255) NOT NULL,
+    phone_number varchar(50),
+    driver_licence varchar(255) NOT NULL
+);
 
--- CREATE TABLE referentiel.vehicle (
---     id SERIAL PRIMARY KEY,
---     year INTEGER
--- );
+--changeset lpuygrenier:002
+CREATE TABLE particulier (
+    id uuid PRIMARY KEY,
+    card_imprint boolean,
+    CONSTRAINT fk_particulier_conducteur FOREIGN KEY (id)
+        REFERENCES conducteur(id)
+);
 
--- CREATE TABLE referentiel.vehicleType (
---     type_name VARCHAR(50)
--- );
+--changeset lpuygrenier:003
+CREATE TABLE entreprise (
+    id uuid PRIMARY KEY,
+    reference varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    phone varchar(50) NOT NULL,
+    address varchar(255) NOT NULL,
+    city varchar(255) NOT NULL,
+    postal_code varchar(20) NOT NULL,
+    siren_siret varchar(50) NOT NULL,
+    naf varchar(50) NOT NULL,
+    tva_number varchar(50) NOT NULL,
+    card_imprint boolean
+);
 
--- CREATE TABLE referentiel.vehicleCoefPrice (
---     coef_price DECIMAL(5,2)
--- );
+--changeset lpuygrenier:004
+CREATE TABLE vehicle (
+    id uuid PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    matricule varchar(50) NOT NULL,
+    commissioning_date date,
+    gas_type uuid NOT NULL,
+    type_id uuid NOT NULL,
+    category_id uuid NOT NULL,
+    kilometer integer,
+    money_earned numeric(10,2),
+    seats integer,
+    color varchar(50)
+);
 
--- CREATE TABLE referentiel.CustomerType (
---     type_name VARCHAR(50)
--- );
+--changeset lpuygrenier:005
+CREATE TABLE payment_method (
+    id integer PRIMARY KEY,
+    label VARCHAR(50) NOT NULL
+);
 
--- CREATE TABLE referentiel.user (
---     id SERIAL PRIMARY KEY,
---     username VARCHAR(50),
---     password VARCHAR(100),
--- );
+CREATE TABLE customer_type (
+    id integer PRIMARY KEY,
+    label VARCHAR(50) NOT NULL
+);
 
--- CREATE TABLE referentiel.vehicleRevision (
---     id SERIAL PRIMARY KEY,
---     vehicle_id INTEGER REFERENCES referentiel.vehicle(id),
---     revision_date DATE,
--- );
 
--- CREATE TABLE referentiel.UtilityCategory (
---     id SERIAL PRIMARY KEY,
---     category_name VARCHAR(50)
--- );
+CREATE TABLE rental_contract (
+    id uuid PRIMARY KEY,
+    reference varchar(255),
+    order_number integer,
+    customer_type_id integer NOT NULL,
+    customer_id uuid NOT NULL,
+    vehicle_id uuid NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    state uuid NOT NULL,
+    kilometer integer,
+    comment text,
+    discount integer,
+    has_snow_tire boolean, 
+    has_trolley boolean,
+    payment_method_id integer NOT NULL,
+    FOREIGN KEY (payment_method_id) REFERENCES payment_method(id)
+    FOREIGN KEY (customer_type_id) REFERENCES customer_type(id)
+    CONSTRAINT fk_rental_vehicle FOREIGN KEY (vehicle_id)
+        REFERENCES vehicle(id)
+);
 
--- CREATE TABLE referentiel.TourismCategory (
---     id SERIAL PRIMARY KEY,
---     category_name VARCHAR(50)
--- );
+CREATE OR REPLACE VIEW rental_contract_view AS
+SELECT
+rc.*,
+1::numeric(10,2) AS base_package,
+1::numeric(10,2) AS base_package_all_kilometers,
+1::numeric(10,2) AS additional_driver,
+1::numeric(10,2) AS snow_tire_amount,
+1::numeric(10,2) AS amount_taxes_free,
+1::numeric(10,2) AS amount_all_taxes
+FROM rental_contract rc;
 
--- CREATE TABLE customer.customer (
---     id SERIAL PRIMARY KEY,
---     first_name VARCHAR(50),
---     last_name VARCHAR(50),
---     email VARCHAR(100),
---     customer_type_id INTEGER REFERENCES referentiel.CustomerType(id)
---     compagny_id INTEGER REFERENCES customer.compagny(id)
--- );
-
--- CREATE TABLE customer.compagny (
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(100),
---     address VARCHAR(200),
--- );
-
--- CREATE TABLE rental.rental (
---     id SERIAL PRIMARY KEY,
---     customer_id INTEGER REFERENCES customer.customer(id),
---     vehicle_id INTEGER REFERENCES referentiel.vehicle(id),
---     start_date DATE,
---     end_date DATE,
---     total_price DECIMAL(10,2)
--- );
+--changeset lpuygrenier:006
+CREATE TABLE rental_contract_drivers (
+    rental_contract_id uuid NOT NULL,
+    driver_id uuid NOT NULL,
+    PRIMARY KEY (rental_contract_id, driver_id),
+    CONSTRAINT fk_rental_contract
+        FOREIGN KEY (rental_contract_id) REFERENCES rental_contract(id),
+    CONSTRAINT fk_driver
+        FOREIGN KEY (driver_id) REFERENCES conducteur(id)
+);
